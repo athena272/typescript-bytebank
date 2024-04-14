0c62e0e871dec5c4e98a1f23d86b4c2112534bd7
athena272 <guilhermera272@gmail.com>
2024-04-14 18:01:03 -0300

    Update typescript.ts

--- ts/typescript.ts
+++ ts/typescript.ts
@@ -2,4 +2,28 @@ let valor: number = 3000;
 let nome: string = "";
 let isPago: boolean = false;
 let qualquer: any = "";
-qualquer = 22; 
+qualquer = 22;
+
+// Arrays
+const lista: number[] = [];
+lista.push(13, 22.5, 22, 89, 1.58);
+
+//Enum
+enum TipoTransacao {
+    DEPOSITO = "Depósito",
+    TRANSFERENCIA = "Transferência",
+    PAGAMENTO_BOLETO = "Pagamento de Boleto"
+}
+
+// Type Alias
+type Transacao = {
+    tipoTransacao: TipoTransacao,
+    data: Date,
+    valor: number,
+}
+
+const novaTransacao: Transacao = {
+    tipoTransacao: TipoTransacao.TRANSFERENCIA,
+    data: new Date(),
+    valor: 999,
+}
