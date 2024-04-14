463095a3b2cc4d2476dca7ed49c371dfa64b120c
athena272 <guilhermera272@gmail.com>
2024-04-14 08:00:32 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -19,79 +19,6 @@ gulp.task('cssApp', function () {
         .pipe(gulp.dest(cssFolder));
 });
 
-// // minify base css
-// gulp.task('cssBase', function () {
-//     console.log("compile cssBase...");
-//     return gulp.src('css/base/_base.css')
-//         .pipe(concat('_base.min.css'))
-//         .pipe(minifyCSS())
-//         .pipe(gulp.dest(baseCssFolder));
-// });
-
-// // minify form css
-// gulp.task('cssForm', function () {
-//     console.log("compile cssForm...");
-//     return gulp.src('css/base/_form.css')
-//         .pipe(concat('_form.min.css'))
-//         .pipe(minifyCSS())
-//         .pipe(gulp.dest(baseCssFolder));
-// });
-
-
-// // minify normalize css
-// gulp.task('cssNormalize', function () {
-//     console.log("compile cssNormalize...");
-//     return gulp.src('css/base/_normalize.css')
-//         .pipe(concat('_normalize.min.css'))
-//         .pipe(minifyCSS())
-//         .pipe(gulp.dest(baseCssFolder));
-// });
-
-// // minify cabecalho css
-// gulp.task('cssCabecalho', function () {
-//     console.log("compile cssCabecalho...");
-//     return gulp.src('css/componentes/_cabecalho.css')
-//         .pipe(concat('_cabecalho.min.css'))
-//         .pipe(minifyCSS())
-//         .pipe(gulp.dest(componentsCssFolder));
-// });
-
-// // minify extrato css
-// gulp.task('cssExtrato', function () {
-//     console.log("compile cssExtrato...");
-//     return gulp.src('css/componentes/_extrato.css')
-//         .pipe(concat('_extrato.min.css'))
-//         .pipe(minifyCSS())
-//         .pipe(gulp.dest(componentsCssFolder));
-// });
-
-// // minify menu css
-// gulp.task('cssMenu', function () {
-//     console.log("compile cssMenu...");
-//     return gulp.src('css/componentes/_menu.css')
-//         .pipe(concat('_menu.min.css'))
-//         .pipe(minifyCSS())
-//         .pipe(gulp.dest(componentsCssFolder));
-// });
-
-// // minify nova transacao css
-// gulp.task('cssNovaTransacao', function () {
-//     console.log("compile cssNovaTransacao...");
-//     return gulp.src('css/componentes/_nova-transacao.css')
-//         .pipe(concat('_nova-transacao.min.css'))
-//         .pipe(minifyCSS())
-//         .pipe(gulp.dest(componentsCssFolder));
-// });
-
-// // minify saldo css
-// gulp.task('cssSaldo', function () {
-//     console.log("compile cssSaldo...");
-//     return gulp.src('css/componentes/_saldo.css')
-//         .pipe(concat('_saldo.min.css'))
-//         .pipe(minifyCSS())
-//         .pipe(gulp.dest(componentsCssFolder));
-// });
-
 gulp.task('bytebankTS', function () {
     console.log("Compilando bytebankTS e minificando bytebankJS...");
     return gulp.src('ts/bytebank.ts')
@@ -129,15 +56,6 @@ gulp.task('bytebankTS', function () {
 // });
 //#endregion
 
-//#region Tasks
-// estrutura basica = gulp.task("NomeDaTarefa", function(){...})
-// src = diretorio do scss a ser compilado
-// pipe = cria ação na compilação
-// sass().on = fica "observando" o sass caso haja erro e apresentado
-// gulp.dest = caminho onde sera salvo o arquivo css já compilado
-
-//#endregion
-
 //  coloca as "tasks" em um array chamado "buildsass"
 gulp.task('buildcss', gulp.series(
     'cssApp',
@@ -148,6 +66,6 @@ gulp.task('buildjs', gulp.series(
 ));
 
 //  cria o comando "gulp watch" que fica observando o array de taks
-// exports.watch = function () {
-//     watch('styles/*.scss', gulp.series('buildsass'));//Verifica arquivos principais
-// };
+exports.watch = function () {
+    watch('ts/*.ts', gulp.series('buildjs'));//Verifica arquivos principais
+};
