5a7b98fe3ca2ad97ee4470059b82c148af870411
athena272 <guilhermera272@gmail.com>
2024-04-14 19:08:07 -0300

    Create novaTransacaoComponente.ts

---
+++ ts/novaTransacaoComponente.ts
@@ -0,0 +1,35 @@
+const elementoFormulario = document.querySelector(".block-nova-transacao form") as HTMLFormElement
+elementoFormulario ? elementoFormulario.addEventListener("submit", function (event) {
+    event.preventDefault()
+    if (!elementoFormulario.checkValidity()) {
+        alert("Por favor, preencha todos os campos")
+        return
+    }
+
+    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao") as HTMLSelectElement
+    const inputValor = elementoFormulario.querySelector("#valor") as HTMLInputElement
+    const inputData = elementoFormulario.querySelector("#data") as HTMLInputElement
+    let tipoTransacao: string = inputTipoTransacao.value
+    let valor: number = inputValor.valueAsNumber
+    let data: Date = new Date(inputData.value)
+
+    // Obtenha o saldo atual usando getSaldo()
+    if (tipoTransacao == "Depósito") {
+        saldo += valor
+    } else if (tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto") {
+        saldo -= valor
+    } else {
+        alert("Tipo de Transação é inválido!")
+        return
+    }
+
+    elementoSaldo ? elementoSaldo.textContent = `R$ ${saldo}` : null
+
+    const novaTransacao = {
+        tipoTransacao,
+        valor,
+        data
+    }
+    console.log("🚀 ~ novaTransacao:", novaTransacao)
+    elementoFormulario.reset()
+}) : null
