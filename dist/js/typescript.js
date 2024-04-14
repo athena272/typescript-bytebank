f4623078fba6e3af32827ea8bf701d1a0f5cb143
athena272 <guilhermera272@gmail.com>
2024-04-14 19:34:09 -0300

    Update typescript.js

--- dist/js/typescript.js
+++ dist/js/typescript.js
@@ -7,15 +7,20 @@ qualquer = 22;
 const lista = [];
 lista.push(13, 22.5, 22, 89, 1.58);
 //Enum
-var TipoTransacao;
-(function (TipoTransacao) {
-    TipoTransacao["DEPOSITO"] = "Dep\u00F3sito";
-    TipoTransacao["TRANSFERENCIA"] = "Transfer\u00EAncia";
-    TipoTransacao["PAGAMENTO_BOLETO"] = "Pagamento de Boleto";
-})(TipoTransacao || (TipoTransacao = {}));
-const novaTransacao = {
-    tipoTransacao: TipoTransacao.PAGAMENTO_BOLETO,
-    data: new Date(),
-    valor: 999,
-};
-console.log("🚀 ~ novaTransacao:", novaTransacao);
+// enum TipoTransacao {
+//     DEPOSITO = "Depósito",
+//     TRANSFERENCIA = "Transferência",
+//     PAGAMENTO_BOLETO = "Pagamento de Boleto"
+// }
+// // Type Alias
+// type Transacao = {
+//     tipoTransacao: TipoTransacao,
+//     data: Date,
+//     valor: number,
+// }
+// const novaTransacao: Transacao = {
+//     tipoTransacao: TipoTransacao.PAGAMENTO_BOLETO,
+//     data: new Date(),
+//     valor: 999,
+// }
+// console.log("🚀 ~ novaTransacao:", novaTransacao)
