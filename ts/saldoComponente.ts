638e28a3098d7cda1c6c3f9a66292c20b4778fd4
athena272 <guilhermera272@gmail.com>
2024-04-16 23:20:55 -0300

    Update saldoComponente.ts

--- ts/saldoComponente.ts
+++ ts/saldoComponente.ts
@@ -1,3 +1,3 @@
 let saldo = 3000
-const elementoSaldo = document.querySelector(".saldo-valor .valor")
-elementoSaldo ? elementoSaldo.textContent = `R$ ${saldo}` : null
+const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement
+elementoSaldo ? elementoSaldo.textContent = saldo.toLocaleString("pt-br", { currency: "BRL", style: "currency" }) : null
