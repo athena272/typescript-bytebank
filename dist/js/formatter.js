94b025b84aa240a410582551af963217e81ebe41
athena272 <guilhermera272@gmail.com>
2024-04-17 02:20:01 -0300

    Create formatter.js

---
+++ dist/js/formatter.js
@@ -0,0 +1,24 @@
+// enum
+var FormatoData;
+(function (FormatoData) {
+    FormatoData["PADRAO"] = "padrao";
+    FormatoData["DIA_SEMANA_DIA_MES_ANO"] = "data longa";
+    FormatoData["DIA_MES"] = "dia mes";
+})(FormatoData || (FormatoData = {}));
+function formatarMoeda(valor) {
+    return valor.toLocaleString("pt-br", { style: "currency", currency: "BRL" });
+}
+function formatarData(data, formato = FormatoData.PADRAO) {
+    if (formato === FormatoData.DIA_SEMANA_DIA_MES_ANO) {
+        return data.toLocaleDateString("pt-br", {
+            weekday: "long",
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric"
+        });
+    }
+    else if (formato === FormatoData.DIA_MES) {
+        return data.toLocaleDateString("pt-br", { day: "2-digit", month: "2-digit" });
+    }
+    return data.toLocaleDateString("pt-br");
+}
