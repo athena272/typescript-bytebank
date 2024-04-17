45f505de4eb3822b235f5ed16e558039af1958d9
athena272 <guilhermera272@gmail.com>
2024-04-16 23:29:02 -0300

    Update saldoComponente.ts

--- ts/saldoComponente.ts
+++ ts/saldoComponente.ts
@@ -1,3 +1,9 @@
 let saldo = 3000
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement
+const elementoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement
+const dataAcesso: Date = new Date()
 elementoSaldo ? elementoSaldo.textContent = saldo.toLocaleString("pt-br", { currency: "BRL", style: "currency" }) : null
+
+elementoDataAcesso.textContent = dataAcesso.toLocaleDateString("pt-br", {
+    weekday: "long", day: "2-digit", month: "2-digit", year: "numeric"
+})
