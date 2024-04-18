464ddc5ce687a66b4b4cb2a32852e4d802dc8bba
athena272 <guilhermera272@gmail.com>
2024-04-18 02:43:31 -0300

    Update saldoComponente.js

--- dist/js/components/saldoComponente.js
+++ dist/js/components/saldoComponente.js
@@ -4,11 +4,12 @@ import Conta from "../types/Conta.js";
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
 const elementoDataAcesso = document.querySelector(".block-saldo time");
 const dataAcesso = Conta.getDataAcesso();
-const saldo = Conta.getSaldo();
 elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO);
 renderSaldo();
 function renderSaldo() {
-    elementoSaldo ? elementoSaldo.textContent = formatarMoeda(saldo) : null;
+    const saldo = Conta.getSaldo();
+    console.log("🚀 ~ renderSaldo(): void", saldo);
+    elementoSaldo.textContent = formatarMoeda(saldo);
 }
 const SaldoComponent = {
     updateInfo() {
