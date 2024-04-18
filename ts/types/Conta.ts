1608fb759b48fef81581f03515f74df54f521ef4
athena272 <guilhermera272@gmail.com>
2024-04-18 02:37:06 -0300

    Create Conta.ts

---
+++ ts/types/Conta.ts
@@ -0,0 +1,29 @@
+import { Transacao, TipoTransacao } from "./Transacao.js";
+
+let saldo = 3000
+
+const Conta = {
+    getSaldo() {
+        return saldo;
+    },
+    getDataAcesso(): Date {
+        return new Date();
+    },
+    registrarTransacao(novaTransacao: Transacao): void {
+        const tipoTransacaoToUse = novaTransacao.tipoTransacao
+        const valorToUse = novaTransacao.valor
+        // Obtenha o saldo atual usando getSaldo()
+        if (tipoTransacaoToUse == "Depósito") {
+            saldo += valorToUse
+        } else if (tipoTransacaoToUse === TipoTransacao.TRANSFERENCIA || tipoTransacaoToUse === TipoTransacao.PAGAMENTO_BOLETO) {
+            saldo -= valorToUse
+        } else {
+            alert("Tipo de Transação é inválido!")
+            return
+        }
+
+        console.log("🚀 ~ registrarTransacao ~ novaTransacao:", novaTransacao)
+    }
+}
+
+export default Conta
