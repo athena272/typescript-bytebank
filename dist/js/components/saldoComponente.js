0a74d2d472a2dbb37b68fb0735e6d843cf640846
athena272 <guilhermera272@gmail.com>
2024-04-18 03:08:33 -0300

    Update saldoComponente.js

--- dist/js/components/saldoComponente.js
+++ dist/js/components/saldoComponente.js
@@ -1,6 +1,7 @@
 import { formatarMoeda } from "../utils/formatter.js";
 import Conta from "../types/Conta.js";
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
+renderSaldo();
 function renderSaldo() {
     const saldo = Conta.getSaldo();
     elementoSaldo.textContent = formatarMoeda(saldo);
