22c03cced9367d7437e730320069d05b30de1ca0
athena272 <guilhermera272@gmail.com>
2024-04-18 03:06:59 -0300

    Create dataComponent.js

---
+++ dist/js/components/dataComponent.js
@@ -0,0 +1,17 @@
+import { formatarData } from "../utils/formatter.js";
+import { FormatoData } from "../types/Data.js";
+import Conta from "../types/Conta.js";
+const elementoDataAcesso = document.querySelector(".block-saldo time");
+renderData();
+function renderData() {
+    const dataAcesso = Conta.getDataAcesso();
+    if (elementoDataAcesso !== null) {
+        elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO);
+    }
+}
+const DataComponent = {
+    updateInfo() {
+        renderData();
+    }
+};
+export default DataComponent;
