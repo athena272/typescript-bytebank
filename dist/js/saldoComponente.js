923b411d54ef2bbfc4d98da7f9c0d161ba5d4f64
athena272 <guilhermera272@gmail.com>
2024-04-17 23:18:57 -0300

    Update saldoComponente.js

--- dist/js/saldoComponente.js
+++ dist/js/saldoComponente.js
@@ -3,4 +3,4 @@ const elementoSaldo = document.querySelector(".saldo-valor .valor");
 const elementoDataAcesso = document.querySelector(".block-saldo time");
 const dataAcesso = new Date();
 elementoSaldo ? elementoSaldo.textContent = formatarMoeda(saldo) : null;
-elementoDataAcesso.textContent = formatarData(dataAcesso);
+elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO);
