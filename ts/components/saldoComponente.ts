0a238d6b89f236516939c66755b1dad7a5fe8acf
athena272 <guilhermera272@gmail.com>
2024-04-18 03:06:53 -0300

    Update saldoComponente.ts

--- ts/components/saldoComponente.ts
+++ ts/components/saldoComponente.ts
@@ -1,14 +1,8 @@
 import { formatarMoeda, formatarData } from "../utils/formatter.js"
-import { FormatoData } from "../types/Data.js"
 import Conta from "../types/Conta.js"
 
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement
-const elementoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement
-const dataAcesso: Date = Conta.getDataAcesso()
 
-elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO)
-
-renderSaldo()
 function renderSaldo(): void {
     const saldo = Conta.getSaldo()
     elementoSaldo.textContent = formatarMoeda(saldo)
