ded9618c699c67825c612d598803aa94cab7c2b7
athena272 <guilhermera272@gmail.com>
2024-04-17 23:13:51 -0300

    Update saldoComponente.js

--- dist/js/saldoComponente.js
+++ dist/js/saldoComponente.js
@@ -2,7 +2,5 @@ let saldo = 3000;
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
 const elementoDataAcesso = document.querySelector(".block-saldo time");
 const dataAcesso = new Date();
-elementoSaldo ? elementoSaldo.textContent = saldo.toLocaleString("pt-br", { currency: "BRL", style: "currency" }) : null;
-elementoDataAcesso.textContent = dataAcesso.toLocaleDateString("pt-br", {
-    weekday: "long", day: "2-digit", month: "2-digit", year: "numeric"
-});
+elementoSaldo ? elementoSaldo.textContent = formatarMoeda(saldo) : null;
+elementoDataAcesso.textContent = formatarData(dataAcesso);
