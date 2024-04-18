f6de30962599384d5085b768e62b123c15a9a27f
athena272 <guilhermera272@gmail.com>
2024-04-18 02:37:18 -0300

    Update saldoComponente.js

--- dist/js/components/saldoComponente.js
+++ dist/js/components/saldoComponente.js
@@ -1,15 +1,18 @@
 import { formatarMoeda, formatarData } from "../utils/formatter.js";
 import { FormatoData } from "../types/Data.js";
-let saldo = 3000;
+import Conta from "../types/Conta.js";
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
 const elementoDataAcesso = document.querySelector(".block-saldo time");
-const dataAcesso = new Date();
+const dataAcesso = Conta.getDataAcesso();
+const saldo = Conta.getSaldo();
 elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO);
-updateSaldo(saldo);
-export function getSaldo() {
-    return saldo;
-}
-export function updateSaldo(novoSaldo) {
-    saldo = novoSaldo;
+renderSaldo();
+function renderSaldo() {
     elementoSaldo ? elementoSaldo.textContent = formatarMoeda(saldo) : null;
 }
+const SaldoComponent = {
+    updateInfo() {
+        renderSaldo();
+    }
+};
+export default SaldoComponent;
