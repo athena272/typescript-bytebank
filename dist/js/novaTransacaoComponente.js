7d5e7508d089e892c025fdb072164d39224e4394
athena272 <guilhermera272@gmail.com>
2024-04-18 02:08:07 -0300

    update js + requisitos

---
+++ dist/js/novaTransacaoComponente.js
@@ -0,0 +1,33 @@
+const elementoFormulario = document.querySelector(".block-nova-transacao form");
+elementoFormulario ? elementoFormulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (!elementoFormulario.checkValidity()) {
+        alert("Por favor, preencha todos os campos");
+        return;
+    }
+    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+    const inputValor = elementoFormulario.querySelector("#valor");
+    const inputData = elementoFormulario.querySelector("#data");
+    let tipoTransacao = inputTipoTransacao.value;
+    let valor = inputValor.valueAsNumber;
+    let data = new Date(inputData.value);
+    // Obtenha o saldo atual usando getSaldo()
+    if (tipoTransacao == "Depósito") {
+        saldo += valor;
+    }
+    else if (tipoTransacao === TipoTransacao.TRANSFERENCIA || tipoTransacao === TipoTransacao.PAGAMENTO_BOLETO) {
+        saldo -= valor;
+    }
+    else {
+        alert("Tipo de Transação é inválido!");
+        return;
+    }
+    elementoSaldo ? elementoSaldo.textContent = formatarMoeda(saldo) : null;
+    const novaTransacao = {
+        tipoTransacao,
+        valor,
+        data
+    };
+    console.log("🚀 ~ novaTransacao:", novaTransacao);
+    elementoFormulario.reset();
+}) : null;
---
+++ dist/js/typescript.js
@@ -0,0 +1,26 @@
+let valor = 3000;
+let nome = "";
+let isPago = false;
+let qualquer = "";
+qualquer = 22;
+// Arrays
+const lista = [];
+lista.push(13, 22.5, 22, 89, 1.58);
+//Enum
+// enum TipoTransacao {
+//     DEPOSITO = "Depósito",
+//     TRANSFERENCIA = "Transferência",
+//     PAGAMENTO_BOLETO = "Pagamento de Boleto"
+// }
+// // Type Alias
+// type Transacao = {
+//     tipoTransacao: TipoTransacao,
+//     data: Date,
+//     valor: number,
+// }
+// const novaTransacao: Transacao = {
+//     tipoTransacao: TipoTransacao.PAGAMENTO_BOLETO,
+//     data: new Date(),
+//     valor: 999,
+// }
+// console.log("🚀 ~ novaTransacao:", novaTransacao)
