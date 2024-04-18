21bc5db4c7bb0b40568153f942e317c31d355bf2
athena272 <guilhermera272@gmail.com>
2024-04-18 02:37:13 -0300

    Update novaTransacaoComponente.ts

--- ts/components/novaTransacaoComponente.ts
+++ ts/components/novaTransacaoComponente.ts
@@ -1,5 +1,6 @@
 import { Transacao, TipoTransacao } from "../types/Transacao.js"
-import { updateSaldo, getSaldo } from "./saldoComponente.js"
+import Conta from "../types/Conta.js"
+import SaldoComponent from "./saldoComponente.js"
 
 const elementoFormulario = document.querySelector(".block-nova-transacao form") as HTMLFormElement
 elementoFormulario ? elementoFormulario.addEventListener("submit", function (event) {
@@ -15,25 +16,16 @@ elementoFormulario ? elementoFormulario.addEventListener("submit", function (eve
     let tipoTransacao: TipoTransacao = inputTipoTransacao.value as TipoTransacao
     let valor: number = inputValor.valueAsNumber
     let data: Date = new Date(inputData.value)
-    let saldo = getSaldo()
-
-    // Obtenha o saldo atual usando getSaldo()
-    if (tipoTransacao == "Depósito") {
-        saldo += valor
-    } else if (tipoTransacao === TipoTransacao.TRANSFERENCIA || tipoTransacao === TipoTransacao.PAGAMENTO_BOLETO) {
-        saldo -= valor
-    } else {
-        alert("Tipo de Transação é inválido!")
-        return
-    }
-
-    updateSaldo(saldo)
 
     const novaTransacao: Transacao = {
         tipoTransacao,
         valor,
         data
     }
-    console.log("🚀 ~ novaTransacao:", novaTransacao)
+
+    Conta.registrarTransacao(novaTransacao)
+
+    SaldoComponent.updateInfo()
+
     elementoFormulario.reset()
 }) : null
