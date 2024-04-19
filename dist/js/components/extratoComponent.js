e382c49bcdaf2cad2f79ab9bc2eab292cd3cdb44
athena272 <guilhermera272@gmail.com>
2024-04-19 00:45:03 -0300

    Update extratoComponent.js

---
+++ dist/js/components/extratoComponent.js
@@ -0,0 +1,40 @@
+import { formatarMoeda, formatarData } from "../utils/formatter.js";
+import { FormatoData } from "../types/Data.js";
+import Conta from "../types/Conta.js";
+const elementoRegistroTransacoesExtrato = document.querySelector(".extrato .registro-transacoes");
+renderizarExtrato();
+function renderizarExtrato() {
+    const gruposTransacoes = Conta.getGruposTransacoes();
+    elementoRegistroTransacoesExtrato.innerHTML = "";
+    let htmlRegistroTransacoes = "";
+    for (let grupoTransacao of gruposTransacoes) {
+        let htmlTransacaoItem = "";
+        for (let transacao of grupoTransacao.transacoes) {
+            htmlTransacaoItem += `
+                <div class="transacao-item">
+                    <div class="transacao-info">
+                        <span class="tipo">${transacao.tipoTransacao}</span>
+                        <strong class="valor">${formatarMoeda(transacao.valor)}</strong>
+                </div>
+                <time class="data">${formatarData(transacao.data, FormatoData.DIA_MES)}</time>
+            </div>
+            `;
+        }
+        htmlRegistroTransacoes += `
+            <div class="transacoes-group">
+                <strong class="mes-group">${grupoTransacao.label}</strong>
+                ${htmlTransacaoItem}
+            </div>
+        `;
+    }
+    if (htmlRegistroTransacoes === "") {
+        htmlRegistroTransacoes = "<div>Não há transações registradas.</div>";
+    }
+    elementoRegistroTransacoesExtrato.innerHTML = htmlRegistroTransacoes;
+}
+const ExtratoComponent = {
+    updateInfo() {
+        renderizarExtrato();
+    }
+};
+export default ExtratoComponent;
