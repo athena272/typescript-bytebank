0e7bfbbbc8352ba0b883587946e2b794086c18e6
athena272 <guilhermera272@gmail.com>
2024-04-19 00:52:13 -0300

    Update novaTransacaoComponente.ts

--- ts/components/novaTransacaoComponente.ts
+++ ts/components/novaTransacaoComponente.ts
@@ -17,7 +17,7 @@ elementoFormulario ? elementoFormulario.addEventListener("submit", function (eve
         const inputData = elementoFormulario.querySelector("#data") as HTMLInputElement
         let tipoTransacao: TipoTransacao = inputTipoTransacao.value as TipoTransacao
         let valor: number = inputValor.valueAsNumber
-        let data: Date = new Date(inputData.value + "00:00:00") // detalhe da linguagem
+        let data: Date = new Date(inputData.value + " 00:00:00") // detalhe da linguagem
 
         const novaTransacao: Transacao = {
             tipoTransacao,
