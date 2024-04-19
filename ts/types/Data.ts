efe1be80e7830f71efd201f28c8e6d71a8ac6952
athena272 <guilhermera272@gmail.com>
2024-04-18 23:50:44 -0300

    Update Data.ts

--- ts/types/Data.ts
+++ ts/types/Data.ts
@@ -1,5 +1,6 @@
 export enum FormatoData {
     PADRAO = "DD/MM/AAAA",
     DIA_SEMANA_DIA_MES_ANO = "DIA_SEMANA, DD/MM/AAAA",
-    DIA_MES = "DD/MM"
+    DIA_MES = "DD/MM",
+    MES_ANO = "MM/AAAA",
 }
