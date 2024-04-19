e9916b30909f9e1f1c0bc05878509e2f6aadac7a
athena272 <guilhermera272@gmail.com>
2024-04-19 00:44:45 -0300

    Update saldoComponente.ts

--- ts/components/saldoComponente.ts
+++ ts/components/saldoComponente.ts
@@ -1,13 +1,32 @@
 import { formatarMoeda, formatarData } from "../utils/formatter.js"
 import Conta from "../types/Conta.js"
 
+function getBlurValue(): string {
+    const saldo = formatarMoeda(Conta.getSaldo())
+    console.log("🚀 ~ getBlurValue ~ saldo:", saldo)
+    const blurSaldo = saldo.replace(/\d/g, 'A')
+    console.log("🚀 ~ getBlurValue ~ blurSaldo:", blurSaldo)
+
+    return blurSaldo
+}
+
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement
+const visualizarSaldo = document.querySelector(".saldo-valor strong img") as HTMLImageElement
 
 renderSaldo()
 function renderSaldo(): void {
     const saldo = Conta.getSaldo()
     elementoSaldo.textContent = formatarMoeda(saldo)
 }
+
+visualizarSaldo.addEventListener("click", () => {
+    elementoSaldo.classList.toggle("blurSaldo")
+    renderSaldo()
+
+    if (elementoSaldo.classList.contains("blurSaldo")) {
+        elementoSaldo.textContent = `${getBlurValue()}`
+    }
+})
 
 const SaldoComponent = {
     updateInfo() {
