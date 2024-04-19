8b8e56a3ec47dc37df27fc192ecfb48b3377ce12
athena272 <guilhermera272@gmail.com>
2024-04-18 21:03:22 -0300

    Update novaTransacaoComponente.js

--- dist/js/components/novaTransacaoComponente.js
+++ dist/js/components/novaTransacaoComponente.js
@@ -2,23 +2,28 @@ import Conta from "../types/Conta.js";
 import SaldoComponent from "./saldoComponente.js";
 const elementoFormulario = document.querySelector(".block-nova-transacao form");
 elementoFormulario ? elementoFormulario.addEventListener("submit", function (event) {
-    event.preventDefault();
-    if (!elementoFormulario.checkValidity()) {
-        alert("Por favor, preencha todos os campos");
-        return;
+    try {
+        event.preventDefault();
+        if (!elementoFormulario.checkValidity()) {
+            alert("Por favor, preencha todos os campos");
+            return;
+        }
+        const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+        const inputValor = elementoFormulario.querySelector("#valor");
+        const inputData = elementoFormulario.querySelector("#data");
+        let tipoTransacao = inputTipoTransacao.value;
+        let valor = inputValor.valueAsNumber;
+        let data = new Date(inputData.value);
+        const novaTransacao = {
+            tipoTransacao,
+            valor,
+            data
+        };
+        Conta.registrarTransacao(novaTransacao);
+        SaldoComponent.updateInfo();
+        elementoFormulario.reset();
     }
-    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
-    const inputValor = elementoFormulario.querySelector("#valor");
-    const inputData = elementoFormulario.querySelector("#data");
-    let tipoTransacao = inputTipoTransacao.value;
-    let valor = inputValor.valueAsNumber;
-    let data = new Date(inputData.value);
-    const novaTransacao = {
-        tipoTransacao,
-        valor,
-        data
-    };
-    Conta.registrarTransacao(novaTransacao);
-    SaldoComponent.updateInfo();
-    elementoFormulario.reset();
+    catch (error) {
+        alert(error.message);
+    }
 }) : null;
