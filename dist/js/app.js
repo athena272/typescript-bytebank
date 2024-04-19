443a92aad7a4b4d705a457a1862996a1850fab46
athena272 <guilhermera272@gmail.com>
2024-04-18 23:27:53 -0300

    Update app.js

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,4 @@
-import "./types/Data.js";
-import "./types/Transacao.js";
-import "./utils/formatter.js";
 import "./components/dataComponent.js";
 import "./components/saldoComponente.js";
 import "./components/novaTransacaoComponente.js";
+import "./components/extratoComponent.js";
