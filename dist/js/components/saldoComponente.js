79c66934c669a5dafbd7565353c874c929fc4eca
athena272 <guilhermera272@gmail.com>
2024-04-19 00:44:59 -0300

    Update saldoComponente.js

--- dist/js/components/saldoComponente.js
+++ dist/js/components/saldoComponente.js
@@ -1,11 +1,26 @@
 import { formatarMoeda } from "../utils/formatter.js";
 import Conta from "../types/Conta.js";
+function getBlurValue() {
+    const saldo = formatarMoeda(Conta.getSaldo());
+    console.log("🚀 ~ getBlurValue ~ saldo:", saldo);
+    const blurSaldo = saldo.replace(/\d/g, 'A');
+    console.log("🚀 ~ getBlurValue ~ blurSaldo:", blurSaldo);
+    return blurSaldo;
+}
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
+const visualizarSaldo = document.querySelector(".saldo-valor strong img");
 renderSaldo();
 function renderSaldo() {
     const saldo = Conta.getSaldo();
     elementoSaldo.textContent = formatarMoeda(saldo);
 }
+visualizarSaldo.addEventListener("click", () => {
+    elementoSaldo.classList.toggle("blurSaldo");
+    renderSaldo();
+    if (elementoSaldo.classList.contains("blurSaldo")) {
+        elementoSaldo.textContent = `${getBlurValue()}`;
+    }
+});
 const SaldoComponent = {
     updateInfo() {
         renderSaldo();
