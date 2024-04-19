54fd84a53c615de6d020a4639198d49c3c0565d2
athena272 <guilhermera272@gmail.com>
2024-04-19 00:44:53 -0300

    Update Data.js

--- dist/js/types/Data.js
+++ dist/js/types/Data.js
@@ -3,4 +3,5 @@ export var FormatoData;
     FormatoData["PADRAO"] = "DD/MM/AAAA";
     FormatoData["DIA_SEMANA_DIA_MES_ANO"] = "DIA_SEMANA, DD/MM/AAAA";
     FormatoData["DIA_MES"] = "DD/MM";
+    FormatoData["MES_ANO"] = "MM/AAAA";
 })(FormatoData || (FormatoData = {}));
