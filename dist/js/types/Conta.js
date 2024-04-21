f49859d0012d6cbddbc38cafe13dabe29cc34053
athena272 <guilhermera272@gmail.com>
2024-04-21 04:23:56 -0300

    Update Conta.js

--- dist/js/types/Conta.js
+++ dist/js/types/Conta.js
@@ -1,3 +1,5 @@
+import { formatarData } from "../utils/formatter.js";
+import { FormatoData } from "./Data.js";
 export class Conta {
     nome;
     saldo = JSON.parse(localStorage.getItem("saldo")) || 0;
@@ -10,6 +12,25 @@ export class Conta {
     constructor({ nome, saldo }) {
         this.nome = nome;
         this.saldo = saldo;
+    }
+    getGruposTransacoes() {
+        const gruposTransacoes = [];
+        // cria uma copia, ao inves de fazer uma referencia na memoria
+        const listaTransacoes = structuredClone(this.transacoes);
+        const transacoesOrdenadas = listaTransacoes.sort((t1, t2) => t2.data.getTime() - t1.data.getTime());
+        let labelAtualGrupoTransacao = '';
+        for (let transacao of transacoesOrdenadas) {
+            let labelGrupoTransacao = formatarData(transacao.data, FormatoData.MES_ANO);
+            if (labelAtualGrupoTransacao !== labelGrupoTransacao) {
+                labelAtualGrupoTransacao = labelGrupoTransacao;
+                gruposTransacoes.push({
+                    label: labelGrupoTransacao,
+                    transacoes: []
+                });
+            }
+            gruposTransacoes.at(-1).transacoes.push(transacao);
+        }
+        return gruposTransacoes;
     }
 }
 export const conta = new Conta({
