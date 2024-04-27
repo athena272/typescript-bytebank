e53aa9b03f90932a5898ef21f61597333afa07d0
athena272 <guilhermera272@gmail.com>
2024-04-27 06:06:25 -0300

    Update Decoratos.js

--- dist/js/types/Decoratos.js
+++ dist/js/types/Decoratos.js
@@ -10,3 +10,13 @@ export function ValidaDebito(target, propertyKey, descriptor) {
         return originalMethod.apply(this, [valorDoDebito]);
     };
 }
+export function ValidaDeposito(target, propertyKey, descriptor) {
+    const originalMethod = descriptor.value;
+    descriptor.value = function (valorDoDeposito) {
+        if (valorDoDeposito <= 0) {
+            throw new Error("O valor a ser depositado deve ser maior do que zero!");
+        }
+        return originalMethod.apply(this, [valorDoDeposito]);
+    };
+    return descriptor;
+}
